0ffaaac9a807cd86b52c05a8a3dd594ab00c0542
Taufan <taufan0264@gmail.com>
2025-10-31 21:39:29 +0700

    Pekan 3 - Tugas 14

--- bioskop/repository/bioskop.go
+++ bioskop/repository/bioskop.go
@@ -7,68 +7,68 @@ import (
 )
 
 func GetAllBioskop(db *sql.DB) (result []structs.Bioskop, err error) {
-    sql := "SELECT id, nama, lokasi, rating FROM bioskop"
+	sql := "SELECT id, nama, lokasi, rating FROM bioskop"
 
-    rows, err := db.Query(sql)
-    if err != nil {
-       return
-    }
+	rows, err := db.Query(sql)
+	if err != nil {
+		return
+	}
 
-    defer rows.Close()
-    for rows.Next() {
-       var bioskop structs.Bioskop
+	defer rows.Close()
+	for rows.Next() {
+		var bioskop structs.Bioskop
 
-       err = rows.Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
-       if err != nil {
-          return
-       }
+		err = rows.Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
+		if err != nil {
+			return
+		}
 
-       result = append(result, bioskop)
-    }
+		result = append(result, bioskop)
+	}
 
-    return
+	return
 }
 
 func GetBioskop(db *sql.DB, bioskop structs.Bioskop) (result structs.Bioskop, err error) {
-    sql := "SELECT id, nama, lokasi, rating  FROM bioskop WHERE id = $1"
+	sql := "SELECT id, nama, lokasi, rating  FROM bioskop WHERE id = $1"
 
-    rows, err := db.Query(sql,  bioskop.ID)
-    if err != nil {
-        return
-    }
-    defer rows.Close()
+	rows, err := db.Query(sql, bioskop.ID)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
 
-    if rows.Next() {
-       err = rows.Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
-        if err != nil {
-            return
-        }
-    } else {
-        return result, fmt.Errorf("bioskop dengan ID %d tidak ditemukan", bioskop.ID)
-    }
+	if rows.Next() {
+		err = rows.Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
+		if err != nil {
+			return
+		}
+	} else {
+		return result, fmt.Errorf("bioskop dengan ID %d tidak ditemukan", bioskop.ID)
+	}
 
-    return result, nil
+	return result, nil
 }
 
 func InsertBioskop(db *sql.DB, bioskop structs.Bioskop) (err error) {
-    sql := "INSERT INTO bioskop(nama, lokasi, rating) VALUES ($1, $2, $3)"
+	sql := "INSERT INTO bioskop(nama, lokasi, rating) VALUES ($1, $2, $3)"
 
-    errs := db.QueryRow(sql, bioskop.Nama, bioskop.Lokasi, bioskop.Rating)
+	errs := db.QueryRow(sql, bioskop.Nama, bioskop.Lokasi, bioskop.Rating)
 
-    return errs.Err()
+	return errs.Err()
 }
 
 func UpdateBioskop(db *sql.DB, bioskop structs.Bioskop) (err error) {
-    sql := "UPDATE bioskop SET nama = $1, lokasi = $2, rating = $4 WHERE id = $3"
+	sql := "UPDATE bioskop SET nama = $1, lokasi = $2, rating = $3 WHERE id = $4"
 
-    errs := db.QueryRow(sql, bioskop.Nama, bioskop.Lokasi, bioskop.Rating, bioskop.ID)
+	errs := db.QueryRow(sql, bioskop.Nama, bioskop.Lokasi, bioskop.Rating, bioskop.ID)
 
-    return errs.Err()
+	return errs.Err()
 }
 
 func DeleteBioskop(db *sql.DB, bioskop structs.Bioskop) (err error) {
-    sql := "DELETE FROM bioskop WHERE id = $1"
+	sql := "DELETE FROM bioskop WHERE id = $1"
 
-    errs := db.QueryRow(sql, bioskop.ID)
-    return errs.Err()
+	errs := db.QueryRow(sql, bioskop.ID)
+	return errs.Err()
 }
